32a76160b2ddfbdb551c83b03fedc662c83a0398
jawadqur <55899496+jawadqur@users.noreply.github.com>
2022-03-08 15:23:52 -0600

    Add email task (#26)

--- bmh_admin_portal_backend/lambda/step_functions_handler/src/handlers/__init__.py
+++ bmh_admin_portal_backend/lambda/step_functions_handler/src/handlers/__init__.py
@@ -1,3 +1,4 @@
 from .provision_brh_handler import ProvisionBRHHandler
 from .failure_handler import FailureHandler
 from .success_handler import SuccessHandler
+from .email_handler import EmailHandler
