6a4dfc026e6b7401ca0d7ac157dd64f65acab73e
Sai Shanmukha Narumanchi <nss10@outlook.com>
2023-01-26 23:01:28 -0600

    Fix typo in log statement

--- bmh_admin_portal_backend/lambda/lambda_authorizer/lambda_authorizer.py
+++ bmh_admin_portal_backend/lambda/lambda_authorizer/lambda_authorizer.py
@@ -48,7 +48,7 @@ def lambda_handler(event, context):
         principalId = f"user|{name}"
 
     else:
-        logger.info("Fetching client_id for client_credentials access_token")
+        logger.info("Fetching client_id from client_credentials access_token")
         client_id = res['azp'] #returns the client id of the decoded token
         context = {"client_id": client_id}
         principalId = f"app|{client_id}"
