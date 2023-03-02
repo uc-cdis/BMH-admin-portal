eababfc6e0697d05c4ad75775e0167db542d6401
tianj7 <jtian1@uchicago.edu>
2023-03-02 09:37:07 -0600

    remove wrong scope for mock dynamodb

--- bmh_admin_portal_backend/tests/conftest.py
+++ bmh_admin_portal_backend/tests/conftest.py
@@ -13,37 +13,53 @@ from unittest.mock import MagicMock, patch
 import boto3
 from moto import mock_dynamodb, mock_ssm
 
+
 @pytest.fixture(autouse=True, scope="session")
 def setup_env_vars():
-    with mock.patch.dict(os.environ, {'dynamodb_table_param_name': 'testTable', 'AWS_DEFAULT_REGION':'us-east-1', 'dynamodb_index_param_name':'testIndex',
-        'email_domain': 'uchicago.edu', 'strides_credits_request_email' : 'occ_test@uchicago.edu', 'state_machine_arn':'testArn',
-        'account_creation_asset_bucket_name': 'testBucket', 'api_usage_id_param_name': 'testUsageId'}):
+    with mock.patch.dict(
+        os.environ,
+        {
+            "dynamodb_table_param_name": "testTable",
+            "AWS_DEFAULT_REGION": "us-east-1",
+            "dynamodb_index_param_name": "testIndex",
+            "email_domain": "uchicago.edu",
+            "strides_credits_request_email": "occ_test@uchicago.edu",
+            "state_machine_arn": "testArn",
+            "account_creation_asset_bucket_name": "testBucket",
+            "api_usage_id_param_name": "testUsageId",
+        },
+    ):
         yield
 
-@pytest.fixture(scope="session")
+
+@pytest.fixture()
 def dynamodb_table():
+
     with mock_dynamodb():
-        dynamodb = boto3.resource('dynamodb')
-        table = dynamodb.create_table(TableName="testTable",
+        dynamodb = boto3.resource("dynamodb")
+        table = dynamodb.create_table(
+            TableName="testTable",
             GlobalSecondaryIndexes=[
                 {
-                    'IndexName': 'testIndex',
-                    'KeySchema': [
-                    {
-                        'AttributeName': 'bmh_workspace_id',
-                        'KeyType': 'HASH'
-                    }
+                    "IndexName": "testIndex",
+                    "KeySchema": [
+                        {"AttributeName": "bmh_workspace_id", "KeyType": "HASH"}
                     ],
-                    'Projection': {
-                    'ProjectionType': 'ALL'
+                    "Projection": {"ProjectionType": "ALL"},
+                    "ProvisionedThroughput": {
+                        "ReadCapacityUnits": 1,
+                        "WriteCapacityUnits": 1,
                     },
-                    'ProvisionedThroughput': {
-                        'ReadCapacityUnits': 1,
-                        'WriteCapacityUnits': 1
-                    }
                 }
             ],
-            KeySchema=[{'AttributeName': 'user_id','KeyType': 'HASH'},{'AttributeName': 'bmh_workspace_id','KeyType': 'RANGE'}],
-            AttributeDefinitions=[{'AttributeName': 'user_id','AttributeType': 'S'},{'AttributeName': 'bmh_workspace_id','AttributeType': 'S'}],
-            ProvisionedThroughput= {'ReadCapacityUnits': 10,'WriteCapacityUnits': 10})
+            KeySchema=[
+                {"AttributeName": "user_id", "KeyType": "HASH"},
+                {"AttributeName": "bmh_workspace_id", "KeyType": "RANGE"},
+            ],
+            AttributeDefinitions=[
+                {"AttributeName": "user_id", "AttributeType": "S"},
+                {"AttributeName": "bmh_workspace_id", "AttributeType": "S"},
+            ],
+            ProvisionedThroughput={"ReadCapacityUnits": 10, "WriteCapacityUnits": 10},
+        )
         yield table
