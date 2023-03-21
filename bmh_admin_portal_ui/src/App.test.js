b108d44d42f426624c8e7bfc742edc98c8f3b5d3
Sai Shanmukha Narumanchi <nss10@outlook.com>
2023-03-21 13:13:35 -0500

    Restrict access to `/admin` for non admin users (#61)

--- bmh_admin_portal_ui/src/App.test.js
+++ bmh_admin_portal_ui/src/App.test.js
@@ -9,15 +9,24 @@ it('renders App component', () => {
 
     expect(appWrapper.find('NavBar')).toHaveLength(1);
     expect(appWrapper.find('LoginCallback')).toHaveLength(1);
+
     const routeList = ["/login", "/login/callback"]
     routeList.forEach((route) =>
     {
       expect(appWrapper.find('Route').find({"path" :route})).toHaveLength(1);
     });
-    expect(appWrapper.find('PrivateRoute')).toHaveLength(3);
-    const privateRouteList = ["/admin", "/request-workspace", "/"]
+    expect(appWrapper.find('PrivateRoute')).toHaveLength(2);
+
+    const privateRouteList = ["/request-workspace", "/"]
     privateRouteList.forEach((route) =>
     {
       expect(appWrapper.find('PrivateRoute').find({"path" :route})).toHaveLength(1);
+    });
+    expect(appWrapper.find('PrivateRoute')).toHaveLength(2);
+
+    const adminRouteList = ["/admin"]
+    adminRouteList.forEach((route) =>
+    {
+      expect(appWrapper.find('AdminRoute').find({"path" :route})).toHaveLength(1);
     });
 });
--- bmh_admin_portal_ui/src/components/admin-route.jsx
+++ bmh_admin_portal_ui/src/components/admin-route.jsx
@@ -4,7 +4,7 @@
 // available at http://aws.amazon.com/agreement or other written agreement between
 // Customer and either Amazon Web Services, Inc. or Amazon Web Services EMEA SARL or both.
 
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Redirect, Route } from 'react-router-dom'
 import {
   authorizeAdmin,
@@ -12,15 +12,20 @@ import {
 import { isAuthenticated } from "../util/oidc"
 
 const AdminRoute = ({ component: Component, ...rest }) => {
-
-  // Add your own authentication on the below line.
-  const isAdmin = authorizeAdmin()
+  const [adminAuthorized, setAdminAuthorized] = useState(false);
+  useEffect(() => {
+    async function fetchAuthorized() {
+      const adminAuthorized = await authorizeAdmin();
+      setAdminAuthorized(adminAuthorized);
+    }
+    fetchAuthorized();
+  }, []);
   const isLoggedIn = isAuthenticated()
   return (
     <Route
       {...rest}
       render={props =>
-        isLoggedIn && isAdmin ? (
+        isLoggedIn && adminAuthorized ? (
           <Component {...props} />
         ) : (
           <Redirect to={{ pathname: '/', state: { from: props.location } }} />
@@ -30,4 +35,4 @@ const AdminRoute = ({ component: Component, ...rest }) => {
   )
 }
 
-export default AdmineRoute
+export default AdminRoute
