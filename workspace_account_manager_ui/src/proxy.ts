9bc9466cba30a198b9f4866891f499de4b2f711f
Mingfei Shao <mshao1@uchicago.edu>
2026-01-13 14:17:08 -0600

    update

--- workspace_account_manager_ui/src/proxy.ts
+++ workspace_account_manager_ui/src/proxy.ts
@@ -1,6 +1,6 @@
 import { NextRequest, NextResponse } from 'next/server';
 
-export function middleware(request: NextRequest) {
+export function proxy(request: NextRequest) {
   const accessToken = request.cookies.get('access_token');
   const { pathname } = request.nextUrl;
 
