85142f80c86e05f7f1ca83677c66f2b0d2832cf3
Mingfei Shao <mshao1@uchicago.edu>
2026-02-23 09:36:55 -0600

    test

--- workspace_account_manager_ui/src/app/login/page.tsx
+++ workspace_account_manager_ui/src/app/login/page.tsx
@@ -46,15 +46,12 @@ function LoginContent() {
         )}
 
         <Stack align="center" gap="md">
-          <Button
+          <button
             onClick={handleLogin}
-            size="lg"
-            variant="filled"
-            fullWidth
-            maw={400}
+            className="group relative w-full flex justify-center py-3 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
           >
             Sign in with SSO
-          </Button>
+          </button>
         </Stack>
       </div>
     </div>
